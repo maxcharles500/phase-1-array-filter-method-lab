7c787881796973386215b161ed390a3ef9fb080d
maximilianjones <maxcharles500@gmail.com>
2021-11-26 11:14:35 -0800

    commit tree 59b5808e56305a5da517ab2b8c1ea9780b42b96c

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+/*function findMatching(arr, string) {
+   let newArr = []
+    if (arr.toLowerCase() === string.toLowerCase()) {
+        return newArr = newArr.push(arr)
+    }
+}
+
+drivers.filter (findMatching(drivers, string)) */
+
+const findMatching = (driverArray, driver) => {
+    //iterate over array
+    //return array that matches string driver
+    //
+    return driverArray.filter(human => {
+        //check where human === driver
+        //case insensit6ive ? make all onecase
+        return human.toLowerCase() === driver.toLowerCase()
+    })
+
+}
+
+const fuzzyMatch = (driverArray, firstLetters) => {
+
+    return driverArray.filter(driver => {
+        const testLength = firstLetters.length
+        return driver.slice(0, testLength).toLowerCase() === firstLetters.toLowerCase()
+    })
+}
+
+const matchName = (driverArray, driver) => {
+
+    return driverArray.filter(driverObj => {
+
+        return driverObj.name === driver
+    })
+}
